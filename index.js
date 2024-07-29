d6c4ad8def17bd3bd8f92d6890ff27285b2f9e70
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-07-29 14:58:38 +0530

    all

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // });
 
 // Security headers
-app.use(helmet());
+app.use(helmet.frameguard({ action: 'deny' }));
 app.use(cookieParser());
 
 // Security headers
